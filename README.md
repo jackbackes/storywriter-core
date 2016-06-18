3a8d01157b8b0cfbbfd97f14aba3f7ccf8eb2275
thejohnbackes <jbackes@thisismotive.com>
2016-06-17 23:56:21 -0400

    added documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Story Writer
+## Welcome
+
+Story writer helps you write stories for kids.
+Based on a corpus of (1) books of children's literature.
--- server/db/_db.js
+++ server/db/_db.js
@@ -2,6 +2,8 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
+
+/** @global */
 var db = new Sequelize(env.DATABASE_URI);
 
 module.exports = db;
