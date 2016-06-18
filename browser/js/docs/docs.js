5f502c352e1442cc88fd4dd7479ead6725175917
thejohnbackes <jbackes@thisismotive.com>
2016-06-18 02:09:04 -0400

    added more docs stuff

--- browser/js/docs/docs.js
+++ browser/js/docs/docs.js
@@ -1,6 +1,6 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('docs', {
         url: '/docs',
-        templateUrl: 'js/docs/docs.html'
+        templateUrl: 'js/docs/docs/index.html'
     });
 });
